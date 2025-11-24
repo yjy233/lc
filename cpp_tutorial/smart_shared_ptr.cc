4b88a85f52a22d3a907c9df3ab1504d96c2daad6
bill <bill@billdeMac-mini.local>
2025-11-24 15:19:46 +0800

    feat: i[date

--- cpp_tutorial/smart_shared_ptr.cc
+++ cpp_tutorial/smart_shared_ptr.cc
@@ -5,31 +5,37 @@ using namespace std;
 
 class MyClass {
   public:
-    int *data;
-    MyClass() {
-        data = new int[100];
+    char *c;
+    MyClass(const string &s) {
+        c = new char[s.size() + 1];
+        strcpy(c, s.c_str());
         cout << "create" << endl;
     }
     ~MyClass() {
-        delete[] data;
+        cout << "delete " << string(this->c) << endl;
+        delete[] c;
         cout << "destroy" << endl;
     }
 };
 
-void show(shared_ptr<MyClass> ptr) {
+void show(shared_ptr<MyClass> &ptr) {
     cout << "Inside show function" << endl;
+    cout << ptr.use_count() << endl;
     // Function that takes shared ownership of a shared_ptr
 }
 
 int main() {
-    // Create a shared_ptr to MyClass
-    shared_ptr<MyClass> sharedPtr1(new MyClass());
-    show(sharedPtr1);
 
+    MyClass *obj = new MyClass("xxx");
+
+    {
+        // Create a shared_ptr to MyClass
+        shared_ptr<MyClass> sharedPtr1(obj);
+        shared_ptr<MyClass> sharedPtr2 = sharedPtr1;
+        show(sharedPtr1);
+        cout << obj->c << endl;
+    }
     // Create another shared_ptr that shares ownership
-    shared_ptr<MyClass> sharedPtr2 = sharedPtr1;
-
-    cout << "Reference count: " << sharedPtr1.use_count() << endl; // Should be 2
-
+    // cout << obj->c << endl;
     return 0;
 }
